9145a3edfb9a446e3c69dd4b84ccd55bc6f23d24
Jamie Popkin <popkinj@littleearth.ca>
2020-06-05 12:25:00 -0700

    BCSans as the default font

--- fpo-api/fpo_api/views.py
+++ fpo-api/fpo_api/views.py
@@ -8,6 +8,10 @@ from api.models.User import User
 
 from api.pdf import render as render_pdf
 
+# For importing our custom font 'BCSans'.
+from weasyprint import HTML, CSS
+from weasyprint.fonts import FontConfiguration
+
 # For testing and development
 from django.views.decorators.csrf import csrf_exempt
 
@@ -27,6 +31,34 @@ def form(request):
     request.GET['name'] -> Care about params????
     request.POST['data'] -> Here is the data
     """
+
+    font_config = FontConfiguration()
+    # Use the BCSans font as the default.
+    css = CSS(string='''
+        @font-face {
+          font-family: 'BCSans';
+          font-style: normal;
+          src: url('https://cdn.jsdelivr.net/npm/@bcgov/bc-sans@1.0.1/fonts/BCSans-Regular.woff') format('woff');
+        }
+        @font-face {
+          font-family: 'BCSans';
+          font-style: italic;
+          src: url('https://cdn.jsdelivr.net/npm/@bcgov/bc-sans@1.0.1/fonts/BCSans-Italic.woff') format('woff');
+        }
+        @font-face {
+          font-family: 'BCSans';
+          font-weight: 700;
+          src: url('https://cdn.jsdelivr.net/npm/@bcgov/bc-sans@1.0.1/fonts/BCSans-Bold.woff') format('woff');
+        }
+        @font-face {
+          font-family: 'BCSans';
+          font-style: italic;
+          font-weight: 700;
+          src: url('https://cdn.jsdelivr.net/npm/@bcgov/bc-sans@1.0.1/fonts/BCSans-BoldItalic.woff') format('woff');
+        }''', font_config=font_config)
+
+
+
     data = json.loads(request.body)
     name = request.GET['name']
     template = '{}.html'.format(name)
@@ -39,6 +71,6 @@ def form(request):
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename="report.pdf"'
 
-    response.write(pdf_content)
+    response.write(pdf_content, stylesheet[css], font_config=font_config)
 
     return response
