11b404b604e00fe1630e610b8d4a4461235e3a6a
Jamie Popkin <popkinj@littleearth.ca>
2020-06-09 12:50:59 -0700

    Removed custom font stuff

--- fpo-api/fpo_api/views.py
+++ fpo-api/fpo_api/views.py
@@ -9,8 +9,8 @@ from api.models.User import User
 from api.pdf import render as render_pdf
 
 # For importing our custom font 'BCSans'.
-from weasyprint import HTML, CSS
-from weasyprint.fonts import FontConfiguration
+#  from weasyprint import HTML, CSS
+#  from weasyprint.fonts import FontConfiguration
 
 # For testing and development
 from django.views.decorators.csrf import csrf_exempt
@@ -32,30 +32,33 @@ def form(request):
     request.POST['data'] -> Here is the data
     """
 
-    font_config = FontConfiguration()
+    #############################################
+    # XXX: Couldn't get this working properly
+    #  font_config = FontConfiguration()
     # Use the BCSans font as the default.
-    css = CSS(string='''
-        @font-face {
-          font-family: 'BCSans';
-          font-style: normal;
-          src: url('https://cdn.jsdelivr.net/npm/@bcgov/bc-sans@1.0.1/fonts/BCSans-Regular.woff') format('woff');
-        }
-        @font-face {
-          font-family: 'BCSans';
-          font-style: italic;
-          src: url('https://cdn.jsdelivr.net/npm/@bcgov/bc-sans@1.0.1/fonts/BCSans-Italic.woff') format('woff');
-        }
-        @font-face {
-          font-family: 'BCSans';
-          font-weight: 700;
-          src: url('https://cdn.jsdelivr.net/npm/@bcgov/bc-sans@1.0.1/fonts/BCSans-Bold.woff') format('woff');
-        }
-        @font-face {
-          font-family: 'BCSans';
-          font-style: italic;
-          font-weight: 700;
-          src: url('https://cdn.jsdelivr.net/npm/@bcgov/bc-sans@1.0.1/fonts/BCSans-BoldItalic.woff') format('woff');
-        }''', font_config=font_config)
+    #  css = CSS(string='''
+        #  @font-face {
+          #  font-family: 'BCSans';
+          #  font-style: normal;
+          #  src: url('https://cdn.jsdelivr.net/npm/@bcgov/bc-sans@1.0.1/fonts/BCSans-Regular.woff') format('woff');
+        #  }
+        #  @font-face {
+          #  font-family: 'BCSans';
+          #  font-style: italic;
+          #  src: url('https://cdn.jsdelivr.net/npm/@bcgov/bc-sans@1.0.1/fonts/BCSans-Italic.woff') format('woff');
+        #  }
+        #  @font-face {
+          #  font-family: 'BCSans';
+          #  font-weight: 700;
+          #  src: url('https://cdn.jsdelivr.net/npm/@bcgov/bc-sans@1.0.1/fonts/BCSans-Bold.woff') format('woff');
+        #  }
+        #  @font-face {
+          #  font-family: 'BCSans';
+          #  font-style: italic;
+          #  font-weight: 700;
+          #  src: url('https://cdn.jsdelivr.net/npm/@bcgov/bc-sans@1.0.1/fonts/BCSans-BoldItalic.woff') format('woff');
+        #  }''', font_config=font_config)
+    #############################################
 
 
 
@@ -71,6 +74,11 @@ def form(request):
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename="report.pdf"'
 
-    response.write(pdf_content, stylesheet[css], font_config=font_config)
+    response.write(pdf_content)
+
+    #############################################
+    # XXX: Couldn't get this working properly
+    #  response.write(pdf_content, stylesheet[css], font_config=font_config)
+    #############################################
 
     return response
