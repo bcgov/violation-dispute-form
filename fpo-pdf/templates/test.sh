7c26d2c840716e16c7c810b1e3716841cb78e9e8
Jamie Popkin <popkinj@littleearth.ca>
2020-06-09 16:25:34 -0700

    Testing new form

--- fpo-pdf/templates/test.sh
+++ fpo-pdf/templates/test.sh
@@ -1,6 +1,6 @@
-curl -X POST --output notice-to-disputant-response.pdf \
+curl -X POST --output violation-ticket-statement-and-written-reasons.pdf \
   -H 'Accept: application/pdf' \
   -H 'Content-Type: text/html' \
-  -d '@notice-to-disputant-response.html' \
-  http://localhost:8083/pdf?filename=notice-to-disputant-response.pdf
+  -d '@violation-ticket-statement-and-written-reasons.html' \
+  http://localhost:8083/pdf?filename=violation-ticket-statement-and-written-reasons.pdf
 
