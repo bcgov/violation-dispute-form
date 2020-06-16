37e4781b00cb1aee0c5f36c2a83130d5bc220e5c
Jamie Popkin <popkinj@littleearth.ca>
2020-06-16 14:54:37 -0700

    Tests working again

--- fpo-api/templates/test.sh
+++ fpo-api/templates/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-  curl -X POST --output notice-to-disputant-response.pdf \
-    -H 'Accept: application/pdf' \
-    -H 'Content-Type: application/json' \
-    -d '@./test-data.json' \
-    "http://localhost:8081/form/?name=notice-to-disputant-response"
-    # "http://localhost:8081/form?name=violation-ticket-statement-and-written-reasons"
+curl -X POST --output notice-to-disputant-response.pdf \
+  -H 'Accept: application/pdf' \
+  -H 'Content-Type: application/json' \
+  -d '@./test-data.json' \
+  "http://localhost:8081/form/?name=notice-to-disputant-response"
+  # "http://localhost:8081/form?name=violation-ticket-statement-and-written-reasons"
