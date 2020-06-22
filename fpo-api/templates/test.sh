370c9886a3b2055dddac1340b9aa46c06a72b08e
Jamie Popkin <popkinj@littleearth.ca>
2020-06-22 13:15:59 -0700

    Dev Env ready

--- fpo-api/templates/test.sh
+++ fpo-api/templates/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-curl -X POST --output notice-to-disputant-response.pdf \
+curl -X POST --output violation-ticket-statement-and-written-reasons.pdf \
   -H 'Accept: application/pdf' \
   -H 'Content-Type: application/json' \
-  -d '@./test-data.json' \
-  "http://localhost:8081/form/?name=notice-to-disputant-response"
-  # "http://localhost:8081/form?name=violation-ticket-statement-and-written-reasons"
+  -d '@./test-data-violation.json' \
+  "http://localhost:8081/form?name=violation-ticket-statement-and-written-reasons"
+  # "http://localhost:8081/form/?name=notice-to-disputant-response"
