dc7e2e7050b2fe21fc5a3318c8d3fe7192289bcb
Jamie Popkin <popkinj@littleearth.ca>
2020-06-22 14:49:33 -0700

    Data for Violation form

--- fpo-api/templates/test.sh
+++ fpo-api/templates/test.sh
@@ -3,5 +3,5 @@ curl -X POST --output violation-ticket-statement-and-written-reasons.pdf \
   -H 'Accept: application/pdf' \
   -H 'Content-Type: application/json' \
   -d '@./test-data-violation.json' \
-  "http://localhost:8081/form?name=violation-ticket-statement-and-written-reasons"
+  "http://localhost:8081/form/?name=violation-ticket-statement-and-written-reasons"
   # "http://localhost:8081/form/?name=notice-to-disputant-response"
