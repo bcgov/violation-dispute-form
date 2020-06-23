a455c142aba6c195e2b03995b91b6a5282524f35
Navpreet Grewal <ngrewal002@gmail.com>
2020-06-23 10:37:50 -0700

    Remove extra lines of code

--- fpo-api/api/send_email.py
+++ fpo-api/api/send_email.py
@@ -29,7 +29,7 @@ def send_email(receiver_email, pdf):
        <b>Thank-you for choosing how you wish to attend your traffic hearing.<b>
        <p>A copy of your completed form is attached for your records.</p>
        <p>Your preference will be reviewed, and a Notice of Hearing will be sent in the mail telling you of the date and time of your hearing. The notice will also tell you whether you will be attending your hearing in-person, by telephone or by video and will provide you with important information about attending your hearing.</p>
-       <p>If you want to learn more about the hearing process, please visit the Provincial Court of British Columbia¿s website and read their Guide to Disputing a Ticket which is found here: https://www.provincialcourt.bc.ca/downloads/Traffic/Traffic Court Guide.pdf
+       <p>If you want to learn more about the hearing process, please visit the Provincial Court of British Columbia¿s website and read their Guide to Disputing a Ticket which is found here: https://www.provincialcourt.bc.ca/downloads/Traffic/Traffic-Court-Guide.pdf
           If you have any questions, please contact the Violation Ticket Centre at: 1-877-661-8026</p>
     </body>
     </html>
@@ -49,14 +49,6 @@ def send_email(receiver_email, pdf):
     
     msg.attach(MIMEText(body, "html"))
 
-
-    #LOGGER.info("blob file is = ", text_file)
-    #REMOVE this when we have real pdf
-    # data_folder = Path("api/")
-    # filename = data_folder/"Responded to attend your Traffic Hearing.pdf"
-    # file = new Blob([pdf], {type: 'application/pdf'});
-    #with open(text_file, "rb") as attachment:
-
     # # Add file as application/octet-stream
     # # Email client can usually download this automatically as attachment
     base=MIMEBase("application", "octet-stream")
@@ -65,8 +57,6 @@ def send_email(receiver_email, pdf):
 
     # # Add header as key/value pair to attachment part
     base.add_header('Content-Disposition','attachment; filename="report.pdf"')
-    #base.add_header("Content-Disposition",f"attachment ;filename= {pdf}")
-    print("message base is =", base)
     # # Add attachment to message and convert message to string
     msg.attach(base)
     text = msg.as_string()
