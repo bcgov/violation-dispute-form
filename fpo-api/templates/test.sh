50700e48b6702ffe065421e965f9507d73ad37ad
Jamie Popkin <popkinj@littleearth.ca>
2020-06-29 13:52:29 -0700

    Page numbers

--- fpo-api/templates/test.sh
+++ fpo-api/templates/test.sh
@@ -3,7 +3,7 @@ curl -X POST --output violation-ticket-statement-and-written-reasons.pdf \
   -H 'Accept: application/pdf' \
   -H 'Content-Type: application/json' \
   -d '@test-data-violation.json' \
-  "http://0.0.0.0:8000/form/?name=violation-ticket-statement-and-written-reasons"
+  "http://0.0.0.0:8081/form/?name=violation-ticket-statement-and-written-reasons"
 
   # "http://localhost:8000/form/?name=violation-ticket-statement-and-written-reasons"
   # "http://localhost:8081/form/?name=notice-to-disputant-response"
