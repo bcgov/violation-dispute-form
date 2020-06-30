4500f54da7ae1b8704e666cd79a3d18307c2275d
Jamie Popkin <popkinj@littleearth.ca>
2020-06-30 13:06:15 -0700

    Fixed footer

--- fpo-api/templates/test.sh
+++ fpo-api/templates/test.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-curl -X POST --output violation-ticket-statement-and-written-reasons.pdf \
+curl -X POST --output notice-to-disputant-response.pdf \
   -H 'Accept: application/pdf' \
   -H 'Content-Type: application/json' \
-  -d '@test-data-violation.json' \
-  "http://0.0.0.0:8081/form/?name=violation-ticket-statement-and-written-reasons"
+  -d '@test-data-notice-in-person.json' \
+  "http://localhost:8081/form/?name=notice-to-disputant-response"
+
+  # "http://0.0.0.0:8081/form/?name=violation-ticket-statement-and-written-reasons"
 
   # "http://localhost:8000/form/?name=violation-ticket-statement-and-written-reasons"
-  # "http://localhost:8081/form/?name=notice-to-disputant-response"
 
