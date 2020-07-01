d72d53aa63a49876b0dc7a3e09b6ea6761c1e325
Travis Semple <travis8814@gmail.com>
2020-06-30 18:49:09 -0700

    Formatting + simplify an interface

--- fpo-web/src/app/interfaces/admin_interfaces.ts
+++ fpo-web/src/app/interfaces/admin_interfaces.ts
@@ -1,103 +1,97 @@
-//This file holds the interfaces for admin. 
+//This file holds the interfaces for admin.
 export interface SearchParameters {
-    filterParameters: FilterParameters;
-    sortParameters: SortParameters;
+  filterParameters: FilterParameters;
+  sortParameters: SortParameters;
 }
-  
+
 export enum AdminPageMode {
-    NewResponse,
-    Archive
+  NewResponse,
+  Archive,
 }
 
 export interface FilterParameters {
-    search: string;
-    createdDate: string;
-    archivedDate: string;
-    region: number;
-    offset: number;
-    page: number; //Unused in Django, we use offset records, not offset page
-    limit: number;
-    isArchived: boolean;
+  search: string;
+  createdDate: string;
+  archivedDate: string;
+  region: number;
+  offset: number;
+  page: number; //Unused in Django, we use offset records, not offset page
+  limit: number;
+  isArchived: boolean;
 }
-  
+
 export interface SortParameters extends Array<SortParameter> {}
-  
+
 export interface SortParameter {
-    prop: string;
-    dir: string;
+  prop: string;
+  dir: string;
 }
 
 export interface SearchResponse {
-    count: number;
-    next: string;
-    previous: string;
-    results: Array<TicketResponseContent>;
+  count: number;
+  next: string;
+  previous: string;
+  results: Array<TicketResponseContent>;
 }
-  
+
 export interface TicketResponseContent {
-    created_date: Date | string;
-    updated_date: Date | string;
-    emailed_date: Date | string;
-   
-    first_name: string;
-    middle_name: string;
-    last_name: string;
-  
-    email: string;
-  
-    hearing_attendance: string;
-    hearing_location: Location;
-  
-    ticket_number: string;
-    ticket_date: Date;
-    deadline_date: Date | string;
-    dispute_type: string;
-    pdf_filename: string;
-    archived_by: ArchivedBy;
-    archived_date: Date | string;
-    printed_by: PrintedBy;
-    printed_date: Date;
+  created_date: Date | string;
+  updated_date: Date | string;
+  emailed_date: Date | string;
+
+  first_name: string;
+  middle_name: string;
+  last_name: string;
+
+  email: string;
+
+  hearing_attendance: string;
+  hearing_location: Location;
+
+  ticket_number: string;
+  ticket_date: Date;
+  deadline_date: Date | string;
+  dispute_type: string;
+  pdf_filename: string;
+  archived_by: User;
+  archived_date: Date | string;
+  printed_by: User;
+  printed_date: Date;
 }
 
-export interface ArchivedBy {
-    first_name: string;
-    last_name: string;
+export interface User {
+  first_name: string;
+  last_name: string;
 }
 
-export interface PrintedBy {
-    first_name: string;
-    last_name: string;
-}
-  
 export interface Location {
-    id: number;
-    name: string;
-    region: Region;
-}
-  
-export interface Region {
-    id: number;
-    name: string;
-}
-  
-export interface ByRegionCount {
-    name: string;
-    id_: number;
-    count: number;
+  id: number;
+  name: string;
+  region: Region;
 }
 
+export interface Region {
+  id: number;
+  name: string;
+}
+
+export interface ByRegionCount {
+  name: string;
+  id_: number;
+  count: number;
+}
 
 export interface TotalCount {
-    count: number;
+  count: number;
 }
-  
+
 export interface RegionCountResponse {
-    new_count: {
-        by_region: Array<ByRegionCount>
-        total: TotalCount
-    },
-    archive_count: {
-        by_region: Array<ByRegionCount>,
-        total: TotalCount
-    }
+  new_count: {
+    by_region: Array<ByRegionCount>;
+    total: TotalCount;
+  };
+  archive_count: {
+    by_region: Array<ByRegionCount>;
+    total: TotalCount;
+  };
 }
