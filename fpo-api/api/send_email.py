77ccb3fe3e498c22b5890324d4c3cbbaf00f8bf0
Navpreet Grewal <ngrewal002@gmail.com>
2020-07-01 20:12:18 -0700

    Fix email error

--- fpo-api/api/send_email.py
+++ fpo-api/api/send_email.py
@@ -29,18 +29,19 @@ def send_email(receiver_email, pdf):
        <b>Thank-you for choosing how you wish to attend your traffic hearing.<b>
        <p>A copy of your completed form is attached for your records.</p>
        <p>Your preference will be reviewed, and a Notice of Hearing will be sent in the mail telling you of the date and time of your hearing. The notice will also tell you whether you will be attending your hearing in-person, by telephone or by video and will provide you with important information about attending your hearing.</p>
-       <p>If you want to learn more about the hearing process, please visit the Provincial Court of British Columbia¿s website and read their Guide to Disputing a Ticket which is found here: https://www.provincialcourt.bc.ca/downloads/Traffic/Traffic-Court-Guide.pdf
+       <p>If you want to learn more about the hearing process, please visit the Provincial Court of British Columbia's website and read their Guide to Disputing a Ticket which is found here: https://www.provincialcourt.bc.ca/downloads/Traffic/Traffic%20Court%20Guide.pdf
           If you have any questions, please contact the Violation Ticket Centre at: 1-877-661-8026</p>
     </body>
     </html>
     """ 
 
     LOGGER.info("User's email Id is  %s>", receiver_email)
-    
-    sender_info = formataddr((str(Header(sender_name, "utf-8")), sender_email))
+
+    if sender_name and sender_email:
+        sender_info = formataddr((str(Header(sender_name, "utf-8")), sender_email))
 
     msg = MIMEMultipart()
-    msg["From"] = sender_email
+    msg["From"] = sender_info
     msg["To"] = receiver_email
     msg["Subject"] = subject
     
