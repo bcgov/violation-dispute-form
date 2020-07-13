61a91d0cd0dcc2598b0510edf24857584e475f53
Wade Barnes <wade.barnes@shaw.ca>
2020-07-13 15:02:04 -0700

    Update proxy settings for api static files.
    - To work with path based proxies.
    
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- fpo-api/fpo_api/settings.py
+++ fpo-api/fpo_api/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = "/api/static/"
+STATIC_URL = "/choose-how-to-attend-your-traffic-hearing/api/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
