f89e5199f260cf7fa734302f3d1eddc29c2154a9
Travis Semple <travis8814@gmail.com>
2020-07-15 13:01:02 -0700

    Add in custom Admins.

--- fpo-api/api/admin.py
+++ fpo-api/api/admin.py
@@ -1,6 +1,72 @@
 from django.contrib import admin
-from api.models.User import User
+from api.models import User, Region, Location
 from django.apps import apps
+
+
+class LocationAdmin(admin.ModelAdmin):
+    '''exclude = [
+        "authorization_guid",
+        "authorization_directory",
+        "authorization_email",
+        "groups",
+        "user_permissions",
+        "password",
+        "display_name",
+        "accepted_terms"
+    ]
+    list_display = [
+        "first_name",
+        "last_name",
+        "email",
+        "is_superuser",
+        "is_staff",
+        "is_active",
+        "date_joined",
+    ]'''
+
+    # Disable adding, because logging in should seed the user.
+    def has_add_permission(self, request):
+        return False
+
+    """ Disable deleting, setting to inactive is better
+    otherwise next time the user uses SSO,
+    they will repopulate. """
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class RegionAdmin(admin.ModelAdmin):
+    '''exclude = [
+        "authorization_guid",
+        "authorization_directory",
+        "authorization_email",
+        "groups",
+        "user_permissions",
+        "password",
+        "display_name",
+        "accepted_terms"
+    ]
+    list_display = [
+        "first_name",
+        "last_name",
+        "email",
+        "is_superuser",
+        "is_staff",
+        "is_active",
+        "date_joined",
+    ]'''
+
+    # Disable adding, because logging in should seed the user.
+    def has_add_permission(self, request):
+        return False
+
+    """ Disable deleting, setting to inactive is better
+    otherwise next time the user uses SSO,
+    they will repopulate. """
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 class UserAdmin(admin.ModelAdmin):
@@ -18,6 +84,7 @@ class UserAdmin(admin.ModelAdmin):
         "first_name",
         "last_name",
         "email",
+        "is_superuser",
         "is_staff",
         "is_active",
         "date_joined",
@@ -41,6 +108,8 @@ for app_config in apps.get_app_configs():
         if admin.site.is_registered(model):
             admin.site.unregister(model)
 
-# Only register user
+# Only register needed.
 admin.site.register(User, UserAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Region, RegionAdmin)
 admin.site.site_header = "VTC Administration"
